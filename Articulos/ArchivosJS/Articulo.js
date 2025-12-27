e559d4250d1f36bab58871f9eb39d8235a4a562a
Mizume25 <gabrielnivicela649@gmail.com>
2025-12-27 23:39:49 +0100

    Commit manual

---
+++ Articulos/ArchivosJS/Articulo.js
@@ -0,0 +1,12 @@
+document.querySelectorAll('.index-list a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+            const targetId = this.getAttribute('href');
+            const targetElement = document.querySelector(targetId);
+                
+            window.scrollTo({
+                 top: targetElement.offsetTop - 20,
+                    behavior: 'smooth'
+                });
+            });
+        });
