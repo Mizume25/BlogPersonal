a7801cdb1243ded58a7188457a131d0496fedd32
Mizume25 <gabrielnivicela649@gmail.com>
2026-01-14 22:02:50 +0100

    ajustes finales

--- Archivador/container/Pagina1.php
+++ Archivador/container/Pagina1.php
@@ -135,53 +135,7 @@
                 </main>
             </div>
         </a>
-        <a href="#"
-            class="card-link"
-            data-id="6"
-            data-categoria="animemanga"
-            data-autor="Miaki Sugaru"
-            data-fecha="2026-01-15"
-            data-destacado="1">
-            <div class="card">
-                <header class="headCard">
-                    <h2>Parasite in Love</h2>
-                    <div class="tagContent">
-                        <div class="tagArticulo">Artículo</div>
-                    </div>
-                </header>
-                <main class="bodyCard">
-                    <section class="bodyimg" style="background-image: url('IMG/Cards/AnimeManga/Sanagi.jpg')"></section>
-                    <section class="bodytxt">
-                        <h3>Todos los organismos deben avanzar</h3>
-                        <p>Analisis de la obra Parasite in love, conjunto de reflexiones primeras....</p>
-                    </section>
-                </main>
-            </div>
-        </a>
-
-        <a href="#"
-            class="card-link"
-            data-id="7"
-            data-categoria="animemanga"
-            data-autor="Yusuke Kishi"
-            data-fecha="2026-01-15"
-            data-destacado="1">
-            <div class="card">
-                <header class="headCard">
-                    <h2>Shin Sekai Yori</h2>
-                    <div class="tagContent">
-                        <div class="tagArticulo">Artículo</div>
-                    </div>
-                </header>
-                <main class="bodyCard">
-                    <section class="bodyimg" style="background-image: url('IMG/Cards/AnimeManga/SSY.jpg')"></section>
-                    <section class="bodytxt">
-                        <h3>La Sinfonía Nº 9 &quot;Del Nuevo Mundo&quot; suena</h3>
-                        <p>Analisis y comentarios generales sobre la obra de Shin Sekai Yori....</p>
-                    </section>
-                </main>
-            </div>
-        </a>
+        
         <?php
         $archivoGenerados = 'cards_generados.html';
         if (file_exists($archivoGenerados)) {
